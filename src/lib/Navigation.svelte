7a40c56a6efd07c4a713203d2cacb7e51d882af2
jill64 <intents.turrets0h@icloud.com>
2023-09-12 23:35:22 +0900

    add lib

--- src/lib/Navigation.svelte
+++ src/lib/Navigation.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
-  export let href: string
+  import inRange from 'lodash/inRange.js'
+
+  export let makeHref: (page: number) => string
   export let show: boolean
   export let label: string
+  export let page: number
+  export let last: number
+
+  $: hidden = show ? !inRange(page, 1, last + 1) : true
 </script>
 
-<a {href} class="paginate-navigation" hidden={show ? null : true}>
+<a href={makeHref(page)} class="paginate-navigation" {hidden}>
   {label}
 </a>
