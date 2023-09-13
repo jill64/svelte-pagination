c4f3e5afc2d0f9a219f156a2846cefa6b6dced96
jill64 <intents.turrets0h@icloud.com>
2023-09-13 15:43:08 +0900

    fix: soft redirect

--- src/routes/+page.server.ts
+++
@@ -1,5 +0,0 @@
-import { redirect } from '@sveltejs/kit'
-
-export const load = () => {
-  throw redirect(301, '/svelte-pagination/1')
-}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { goto } from '$app/navigation'
+  import { base } from '$app/paths'
+  import { onMount } from 'svelte'
+
+  onMount(() => goto(`${base}/1`))
+</script>
