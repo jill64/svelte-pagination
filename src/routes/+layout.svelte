70ddd437592871174a277f3839607a1b41a8a439
jill64 <143883742+jill64@users.noreply.github.com>
2023-10-24 13:16:43 +0000

    add github link in demo header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,2 +1,21 @@
-<h1>svelte-pagination</h1>
+<header>
+  <h1>svelte-pagination</h1>
+  <a href="https://github.com/jill64/svelte-pagination">GitHub</a>
+</header>
 <slot />
+
+<style>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 0.5rem;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+</style>
