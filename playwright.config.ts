3cfcd512587867f84458d9b4365776fcaf6ecabb
jill64 <intents.turrets0h@icloud.com>
2023-10-24 22:47:58 +0900

    Revert "fix: test config for CI"
    This reverts commit 608e1734367ef83e9b9875bcb4663fdc384d8580.

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   },
   testDir: 'tests',
   fullyParallel: true,
-  workers: process.env.CI ? 1 : '100%',
+  workers: '100%',
   retries: process.env.CI ? 2 : 0,
   projects: [
     {
