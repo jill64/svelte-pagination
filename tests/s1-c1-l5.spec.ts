f3841538f1797144a59be52686715577080f4166
jill64 <intents.turrets0h@icloud.com>
2023-11-04 16:22:24 +0900

    split test

---
+++ tests/s1-c1-l5.spec.ts
@@ -0,0 +1,59 @@
+import { expect, test } from '@playwright/test'
+
+test('side:1 center:1 last:5', async ({ page }) => {
+  await page.goto('/1')
+
+  await page.getByPlaceholder('Side Size').fill('1')
+  await page.getByPlaceholder('Center Size').fill('1')
+  await page.getByPlaceholder('Last Page').fill('5')
+
+  await expect(page.locator('span').filter({ hasText: '1' })).toBeVisible()
+  await expect(
+    page.getByRole('link', { name: '2', exact: true })
+  ).not.toBeVisible()
+  await expect(
+    page.getByRole('link', { name: '3', exact: true })
+  ).not.toBeVisible()
+  await expect(
+    page.getByRole('link', { name: '4', exact: true })
+  ).not.toBeVisible()
+  await expect(page.getByRole('link', { name: '5', exact: true })).toBeVisible()
+  await expect(page.getByRole('link', { name: 'Next' })).toBeVisible()
+
+  await page.goto('/svelte-pagination/2')
+
+  await page.getByPlaceholder('Side Size').fill('1')
+  await page.getByPlaceholder('Center Size').fill('1')
+  await page.getByPlaceholder('Last Page').fill('5')
+
+  await expect(page.getByRole('link', { name: 'Previous' })).toBeVisible()
+  await expect(page.getByRole('link', { name: '1', exact: true })).toBeVisible()
+  await expect(page.locator('span').filter({ hasText: '2' })).toBeVisible()
+  await expect(
+    page.getByRole('link', { name: '3', exact: true })
+  ).not.toBeVisible()
+  await expect(
+    page.getByRole('link', { name: '4', exact: true })
+  ).not.toBeVisible()
+  await expect(page.getByRole('link', { name: '5', exact: true })).toBeVisible()
+  await expect(page.getByRole('link', { name: 'Next' })).toBeVisible()
+
+  await page.goto('/svelte-pagination/5')
+
+  await page.getByPlaceholder('Side Size').fill('1')
+  await page.getByPlaceholder('Center Size').fill('1')
+  await page.getByPlaceholder('Last Page').fill('5')
+
+  await expect(page.getByRole('link', { name: 'Previous' })).toBeVisible()
+  await expect(page.getByRole('link', { name: '1', exact: true })).toBeVisible()
+  await expect(
+    page.getByRole('link', { name: '2', exact: true })
+  ).not.toBeVisible()
+  await expect(
+    page.getByRole('link', { name: '3', exact: true })
+  ).not.toBeVisible()
+  await expect(
+    page.getByRole('link', { name: '4', exact: true })
+  ).not.toBeVisible()
+  await expect(page.locator('span').filter({ hasText: '5' })).toBeVisible()
+})
---
+++ tests/s2-c3-l10.spec.ts
@@ -0,0 +1,60 @@
+import { expect, test } from '@playwright/test'
+
+test('side:2 center:3 last:10 (Default)', async ({ page }) => {
+  await page.goto('/1')
+
+  await expect(page.locator('span').filter({ hasText: '1' })).toBeVisible()
+  await expect(page.getByRole('link', { name: '2', exact: true })).toBeVisible()
+  await expect(
+    page.getByRole('link', { name: '3', exact: true })
+  ).not.toBeVisible()
+
+  await expect(
+    page.getByRole('link', { name: '8', exact: true })
+  ).not.toBeVisible()
+  await expect(page.getByRole('link', { name: '9', exact: true })).toBeVisible()
+  await expect(
+    page.getByRole('link', { name: '10', exact: true })
+  ).toBeVisible()
+  await expect(page.getByRole('link', { name: 'Next' })).toBeVisible()
+
+  await page.goto('/svelte-pagination/5')
+
+  await expect(page.getByRole('link', { name: 'Previous' })).toBeVisible()
+  await expect(page.getByRole('link', { name: '1', exact: true })).toBeVisible()
+  await expect(page.getByRole('link', { name: '2', exact: true })).toBeVisible()
+  await expect(
+    page.getByRole('link', { name: '3', exact: true })
+  ).not.toBeVisible()
+
+  await expect(page.getByRole('link', { name: '4', exact: true })).toBeVisible()
+  await expect(page.locator('span').filter({ hasText: '5' })).toBeVisible()
+  await expect(page.getByRole('link', { name: '6', exact: true })).toBeVisible()
+  await expect(
+    page.getByRole('link', { name: '7', exact: true })
+  ).not.toBeVisible()
+
+  await expect(
+    page.getByRole('link', { name: '8', exact: true })
+  ).not.toBeVisible()
+  await expect(page.getByRole('link', { name: '9', exact: true })).toBeVisible()
+  await expect(
+    page.getByRole('link', { name: '10', exact: true })
+  ).toBeVisible()
+  await expect(page.getByRole('link', { name: 'Next' })).toBeVisible()
+
+  await page.goto('/svelte-pagination/10')
+
+  await expect(page.getByRole('link', { name: 'Previous' })).toBeVisible()
+  await expect(page.getByRole('link', { name: '1', exact: true })).toBeVisible()
+  await expect(page.getByRole('link', { name: '2', exact: true })).toBeVisible()
+  await expect(
+    page.getByRole('link', { name: '3', exact: true })
+  ).not.toBeVisible()
+
+  await expect(
+    page.getByRole('link', { name: '8', exact: true })
+  ).not.toBeVisible()
+  await expect(page.getByRole('link', { name: '9', exact: true })).toBeVisible()
+  await expect(page.locator('span').filter({ hasText: '10' })).toBeVisible()
+})
